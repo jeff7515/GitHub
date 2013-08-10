8f8515e25f994ded9c9668eb8382d0718be0e899
jeff7515 <jeff_7515@yahoo.com.tw>
2013-08-09 21:14:25 -0400

    A version2

--- ClassTest/src/com/me/main/A.java
+++ ClassTest/src/com/me/main/A.java
@@ -16,43 +16,8 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 
-public class A implements Lock {
-	@Override
-	public void lock() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void lockInterruptibly() throws InterruptedException {
-		// TODO Auto-generated method stub
-	}
-
-	@Override
-	public Condition newCondition() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean tryLock() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean tryLock(long time, TimeUnit unit)
-			throws InterruptedException {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public void unlock() {
-		// TODO Auto-generated method stub
-
-	}
-
+public class A {
+	
 	public static void main(String[] args) {
 
 		ArrayList<String> list = new ArrayList<String>();
